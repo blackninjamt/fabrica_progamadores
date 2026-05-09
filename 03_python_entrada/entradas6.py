2e9956b9f7472a61d741e5f117ee5a7a0e1290f8
blackninjamt <avilasouzab@gmail.com>
2026-05-09 11:42:43 -0300

    Add files via upload

---
+++ 03_python_entrada/entradas6.py
@@ -0,0 +1,9 @@
+#Autor: Bryan de Avila Souza 
+#Projeto: IMC
+
+nome = input("DIGITE SEU NOME:")
+peso = float(input("Digite seu peso:"))
+altura = float(input("digite sua altura:"))
+resultado = peso / ( altura * altura)
+
+print(f"o resultado do seu IMC é:{resultado}")
